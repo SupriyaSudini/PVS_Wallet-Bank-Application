96c520369459be4ff618c57b2909379a9b86e0f0
SupriyaSudini <supriyasudini165@gmail.com>
2024-03-11 13:09:39 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+app.use(express.json());
+const dbConfig = require('./config/dbConfig.js');
+const usersRoute = require('./routes/userRoute.js');
+const transactionsRoute = require('./routes/transactionsRoutes.js');
+const requestsRoute = require('./routes/requestsRoute.js');
+
+// entry point for user route
+app.use('/api/users', usersRoute);
+
+//entry point for transaction route
+app.use('/api/transactions', transactionsRoute);
+
+// call the end points in routes folder here 
+app.use('/api/requests', requestsRoute);
+
+const PORT = process.env.PORT || 5000;
+const path = require("path");
+
+// heroku deployment
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("client/build"));
+    app.get("*",(req, res)=>{
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
+
+app.listen(PORT, () => {
+    console.log(`Server started on Port ${PORT}`);
+});
+
